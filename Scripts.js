ac6cbedf4f7920182b52d098ecac81c3e959be71
Intelliwebchatt <aslockhart10@gmail.com>
2024-10-07 13:14:48 -0500

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,4 +1,4 @@
-// Smooth Scrolling for Internal Links
+Smooth Scrolling for Internal Links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
